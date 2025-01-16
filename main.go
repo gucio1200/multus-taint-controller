d14b9ac149fb72dbe3feed1cdb8583918190a56d
gucio1200 <kacper.gogol@gmail.com>
2025-01-16 14:24:56 +0100

    Update

--- main.go
+++ main.go
@@ -8,10 +8,6 @@ import (
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
-	"k8s.io/client-go/tools/clientcmd"
-	"k8s.io/client-go/util/retry"
-        "k8s.io/apimachinery/pkg/util/wait"
-	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
 )
 
