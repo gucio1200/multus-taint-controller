0ccd9ad03080b1f8da55d79b08312de8c5bc3114
gucio1200 <kacper.gogol@gmail.com>
2025-07-10 00:40:17 +0200

    Update main.go

--- main.go
+++ main.go
@@ -10,21 +10,21 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
+	"k8s.io/client-go/util/retry"
 	"k8s.io/klog/v2"
 )
 
 var (
-	labelSelector  = os.Getenv("MULTUS_LABEL_SELECTOR")
-	taintKey       = "multus.network.k8s.io/readiness"
-	taintValue     = "false"
-	taintEffect    = corev1.TaintEffectNoSchedule
+	labelSelector = os.Getenv("MULTUS_LABEL_SELECTOR")
+	taintKey      = "multus.network.k8s.io/readiness"
+	taintValue    = "false"
+	taintEffect   = corev1.TaintEffectNoSchedule
 )
 
 func main() {
 	klog.InitFlags(nil)
 	defer klog.Flush()
 
-	// In-cluster configuration for authenticating to Kubernetes API
 	config, err := rest.InClusterConfig()
 	if err != nil {
 		panic(fmt.Sprintf("Error creating in-cluster config: %s", err))
@@ -35,19 +35,15 @@ func main() {
 		panic(fmt.Sprintf("Error creating clientset: %s", err))
 	}
 
-	// Dynamically read the namespace from the file provided by Kubernetes
 	namespace, err := ioutil.ReadFile("/var/run/secrets/kubernetes.io/serviceaccount/namespace")
 	if err != nil {
 		panic(fmt.Sprintf("Error reading namespace from file: %s", err))
 	}
 
-	// Run the controller logic
 	runController(clientset, string(namespace))
 }
 
-// runController contains the logic for the controller
 func runController(clientset *kubernetes.Clientset, namespace string) {
-	// Watch nodes for changes
 	nodeWatcher, err := clientset.CoreV1().Nodes().Watch(context.Background(), metav1.ListOptions{})
 	if err != nil {
 		panic(fmt.Sprintf("Error watching nodes: %s", err))
@@ -57,7 +53,6 @@ func runController(clientset *kubernetes.Clientset, namespace string) {
 		node := event.Object.(*corev1.Node)
 		nodeName := node.Name
 
-		// Check if Multus is ready and taint/untaint node accordingly
 		if isMultusReady(clientset) {
 			untaintNode(clientset, nodeName)
 		} else {
@@ -66,7 +61,6 @@ func runController(clientset *kubernetes.Clientset, namespace string) {
 	}
 }
 
-// Check if Multus pod is running
 func isMultusReady(clientset *kubernetes.Clientset) bool {
 	pods, err := clientset.CoreV1().Pods("").List(context.Background(), metav1.ListOptions{
 		LabelSelector: labelSelector,
@@ -79,54 +73,57 @@ func isMultusReady(clientset *kubernetes.Clientset) bool {
 	return len(pods.Items) > 0
 }
 
-// Taint the node if Multus is not ready
 func taintNode(clientset *kubernetes.Clientset, nodeName string) {
-	node, err := clientset.CoreV1().Nodes().Get(context.Background(), nodeName, metav1.GetOptions{})
-	if err != nil {
-		fmt.Printf("Error getting node %s: %v\n", nodeName, err)
-		return
-	}
-
-	// Taint the node if not already tainted
-	for _, taint := range node.Spec.Taints {
-		if taint.Key == taintKey {
-			return // Node already tainted
+	err := retry.RetryOnConflict(retry.DefaultRetry, func() error {
+		node, err := clientset.CoreV1().Nodes().Get(context.Background(), nodeName, metav1.GetOptions{})
+		if err != nil {
+			fmt.Printf("Error getting node %s: %v\n", nodeName, err)
+			return err
 		}
-	}
 
-	taint := corev1.Taint{
-		Key:    taintKey,
-		Value:  taintValue,
-		Effect: taintEffect,
-	}
-	nodeCopy := node.DeepCopy()
-	nodeCopy.Spec.Taints = append(nodeCopy.Spec.Taints, taint)
+		for _, taint := range node.Spec.Taints {
+			if taint.Key == taintKey {
+				return nil // Already tainted
+			}
+		}
 
-	_, err = clientset.CoreV1().Nodes().Update(context.Background(), nodeCopy, metav1.UpdateOptions{})
+		nodeCopy := node.DeepCopy()
+		nodeCopy.Spec.Taints = append(nodeCopy.Spec.Taints, corev1.Taint{
+			Key:    taintKey,
+			Value:  taintValue,
+			Effect: taintEffect,
+		})
+
+		_, err = clientset.CoreV1().Nodes().Update(context.Background(), nodeCopy, metav1.UpdateOptions{})
+		return err
+	})
+
 	if err != nil {
 		fmt.Printf("Error tainting node %s: %v\n", nodeName, err)
 	}
 }
 
-// Remove the taint from the node if Multus is ready
 func untaintNode(clientset *kubernetes.Clientset, nodeName string) {
-	node, err := clientset.CoreV1().Nodes().Get(context.Background(), nodeName, metav1.GetOptions{})
-	if err != nil {
-		fmt.Printf("Error getting node %s: %v\n", nodeName, err)
-		return
-	}
-
-	// Remove the taint
-	nodeCopy := node.DeepCopy()
-	var taints []corev1.Taint
-	for _, taint := range nodeCopy.Spec.Taints {
-		if taint.Key != taintKey {
-			taints = append(taints, taint)
+	err := retry.RetryOnConflict(retry.DefaultRetry, func() error {
+		node, err := clientset.CoreV1().Nodes().Get(context.Background(), nodeName, metav1.GetOptions{})
+		if err != nil {
+			fmt.Printf("Error getting node %s: %v\n", nodeName, err)
+			return err
 		}
-	}
-	nodeCopy.Spec.Taints = taints
 
-	_, err = clientset.CoreV1().Nodes().Update(context.Background(), nodeCopy, metav1.UpdateOptions{})
+		nodeCopy := node.DeepCopy()
+		var newTaints []corev1.Taint
+		for _, taint := range nodeCopy.Spec.Taints {
+			if taint.Key != taintKey {
+				newTaints = append(newTaints, taint)
+			}
+		}
+		nodeCopy.Spec.Taints = newTaints
+
+		_, err = clientset.CoreV1().Nodes().Update(context.Background(), nodeCopy, metav1.UpdateOptions{})
+		return err
+	})
+
 	if err != nil {
 		fmt.Printf("Error removing taint from node %s: %v\n", nodeName, err)
 	}
